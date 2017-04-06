d31bc4202d640a14df041443a5e7c993e478d3f7
John Wujciak <jmw2257@gmail.com>
2017-04-06 03:16:34 +0000

    Added web form to create an article. Need to create show next

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ root 'pages#home'
   
 get 'about', to: 'pages#about'
 
+resources :articles
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
